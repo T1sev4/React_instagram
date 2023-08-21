92d6d220d5cd58c2e2ace77d6bacb3a126b278d3
t1sev4 <tamikofficial@gmail.com>
2023-08-21 20:44:37 +0600

    add modal window

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,11 +1,18 @@
+'use client'
+
 import Profile from "@/components/myprofile"
 import Header from "@/components/header"
+import ModalWindow from "@/components/modal-window"
+import { useState } from 'react';
 
 export default function Login() {
+  const [isModalOpen, setModalOpen] = useState(false); // Состояние для модального окна
+
   return (
     <main>
-      <Header />
+      <Header openModal={() => setModalOpen(true)} />
       <Profile />
+      {isModalOpen && <ModalWindow closeModal={() => setModalOpen(false)} />}
     </main>
   )
 }
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
 
-export default function Header(){
+export default function Header({openModal}){
   return(
     <header className="header ptb4">
       <div className="container flex flex-jc-sb flex-ai-c">
@@ -15,7 +15,7 @@ export default function Header(){
         <div className="header_icons flex">
           <img src='/images/header/home.svg' alt='' />
           <img src='/images/header/message.svg' alt='' />
-          <img src='/images/header/NewPosts.svg' alt='' />
+          <img onClick={openModal} src='/images/header/NewPosts.svg' alt='' />
           <img src='/images/header/FindPeople.svg' alt='' />
           <img src='/images/header/heart.svg' alt='' />
           <img src='https://w.forfun.com/fetch/39/399f74771a37da97630f86e85696f882.jpeg' alt='' />
